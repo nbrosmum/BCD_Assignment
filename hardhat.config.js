f72832fe44fb7bdfe479bfda103962ff42cb4898
nbrosmum <103727624+nbrosmum@users.noreply.github.com>
2025-03-30 12:46:43 +0800

    update the .env file add the sepolia testnet and private key for meta mask sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,12 @@
 /** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
